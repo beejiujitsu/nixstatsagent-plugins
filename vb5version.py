08b8891d7d5303d9d54b79a35513f03ce79f86f4
Mark Shields <laebshade@gmail.com>
2020-09-24 19:59:51 -0400

    move helper functions back to class

--- vb5version.py
+++ vb5version.py
@@ -10,37 +10,32 @@ class Plugin(plugins.BasePlugin):
     __name__ = "vb5version"
 
     def run(self, *_) -> Dict[str, str]:
-        _current = to_float(current())
-        _latest = to_float(latest())
+        _current = self.to_float(self.current()) or "-1"
+        _latest = self.to_float(self.latest()) or "-1"
         using_latest = str(int(_current == _latest))
         return {"current": _current, "latest": _latest, "using_latest": using_latest}
 
+    def to_float(self, version: str) -> str:
+        parts = version.split(".")
+        try:
+            return "%s.%s%s" % (parts[0], parts[1], parts[2])
+        except IndexError:
+            return version
 
-def to_float(version: str) -> str:
-    parts = version.split(".")
-    try:
-        return "%s.%s%s" % (parts[0], parts[1], parts[2])
-    except IndexError:
-        return version
+    def current(self) -> str:
+        with open("/var/www/html/index.php") as fh:
+            for line in fh.readlines():
+                if "vBulletin 5" in line:
+                    return line.split()[3]
 
-
-def current() -> str:
-    with open("/var/www/html/index.php") as fh:
-        for line in fh.readlines():
-            if "vBulletin 5" in line:
-                return line.split()[3]
-    return "-1"
-
-
-def latest() -> str:
-    f = requests.get("https://www.vbulletin.com/download.php")
-    myre = re.compile(r"(5\.\d+\.\d+)")
-    for line in f.text.splitlines():
-        if "Latest Version:" in line:
-            matched = myre.search(line)
-            if matched:
-                return matched.group(0)
-    return "-1"
+    def latest(self) -> str:
+        f = requests.get("https://www.vbulletin.com/download.php")
+        myre = re.compile(r"(5\.\d+\.\d+)")
+        for line in f.text.splitlines():
+            if "Latest Version:" in line:
+                matched = myre.search(line)
+                if matched:
+                    return matched.group(0)
 
 
 if __name__ == "__main__":
