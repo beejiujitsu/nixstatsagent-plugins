5d3cc306eb2e9bba7a9ced486b6baa145a519af0
Mark Shields <laebshade@gmail.com>
2020-09-25 17:34:43 -0400

    download.php deprecated, so parse vbulletin forum

--- vb5version.py
+++ vb5version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from contextlib import suppress
 import re
-from typing import Dict
+from typing import Dict, List
 
 import plugins
 import requests
@@ -13,29 +13,42 @@ class Plugin(plugins.BasePlugin):
     def run(self, *_) -> Dict[str, str]:
         _current = self.to_float(self.current()) or "-1"
         _latest = self.to_float(self.latest()) or "-1"
-        using_latest = str(int(_current == _latest))
-        return {"current": _current, "latest": _latest, "using_latest": using_latest}
+        _using_latest = self.using_latest(_current, _latest)
+        return {"current": _current, "latest": _latest, "using_latest": _using_latest}
 
     def to_float(self, version: str) -> str:
         try:
-            parts = version.split(".")
-            return "%s.%s%s" % (parts[0], parts[1], parts[2])
+            split = version.split(".")
+            return f"{split[0]}.{split[1]}{split[2]}"
         except Exception:
             return version
+
+    def using_latest(self, _current, _latest) -> str:
+        return str(
+            int(
+                all([_current == _latest, _current != "-1", _latest != "-1"])
+            )
+        )
 
     def current(self) -> str:
         with open("/var/www/html/index.php") as fh:
             for line in fh.readlines():
-                if "vBulletin 5" in line:
+                if "vbulletin 5" in line.lower():
                     return line.split()[3]
 
     def latest(self) -> str:
-        myre = re.compile(r"(5\.\d+\.\d+)")
-        for line in requests.get("https://www.vbulletin.com/download.php").text.splitlines():
-            if "Latest Version:" in line:
-                match = myre.search(line)
+        prog = re.compile(r"(5\.\d+\.\d+)")
+        sec = re.compile(r"(?<=Security Patch Level )(\d+)")
+        for line in requests.get("https://forum.vbulletin.com/forum/vbulletin-announcements/vbulletin-announcements_aa").text.splitlines():
+            if "vbulletin connect" in line.lower():
+                match = prog.search(line)
                 if match:
                     return match.group(0)
+            if "vbulletin" in line.lower() and "security patch" in line.lower():
+                match = prog.search(line) 
+                sec_match = sec.search(line)
+                if match and sec_match:
+                    return match.group(0) + sec_match.group(0)
 
 
 if __name__ == "__main__":
